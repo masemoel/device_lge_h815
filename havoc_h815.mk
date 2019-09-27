702b5e4356607d7eee7540a8f3dd0d15d744de68
aoleary <aoleary@gmail.com>
2019-09-27 13:21:00 +0000

    Update RIL props

--- havoc_h815.mk
+++ havoc_h815.mk
@@ -39,7 +39,16 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
-    PRODUCT_NAME="g4_global_com" \
-    PRIVATE_BUILD_DESC="p1_global_com-user 8.0 NRD90U 171931902b165 release-keys"
+    PRODUCT_NAME="p1_global_com" \
+    PRIVATE_BUILD_DESC="p1_global_com-user 6.0 MRA58K 152940055675e release-keys"
 
-BUILD_FINGERPRINT := "lge/p1_global_com/p1:8.1.0/OPM4.171019.016.A1/4720815:user/release-keys"
+BUILD_FINGERPRINT := "lge/p1_global_com/p1:6.0/MRA58K/152940055675e:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.do_not_init_csvt=1 \
+    rild.libargs=-d /dev/smd0 \
+    ril.subscription.types=NV,RUIM \
+    ro.telephony.call_ring.multiple=0 \
+    ro.telephony.default_network=12 \
+    telephony.lteOnCdmaDevice=0 \
+    telephony.lteOnGsmDevice=1
